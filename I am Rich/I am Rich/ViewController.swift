ed27de31cc57dd737f5510731d02d3236a7fcc3f
Tadjoudine <mtadjdine@yahoo.com>
2025-10-15 18:48:49 -0500

    I am Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Tadjoudine Mouhamed on 9/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
