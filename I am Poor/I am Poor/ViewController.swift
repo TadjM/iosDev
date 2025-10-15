f54b6b77cba4ad17b3b5ecce1a8ecf9d8a1f7fcc
Tadjoudine <mtadjdine@yahoo.com>
2025-10-15 18:46:27 -0500

    I am Poor

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Tadjoudine Mouhamed on 9/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
